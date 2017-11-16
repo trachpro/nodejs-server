9521b59e2c49281d945417a077a89ed1a946e0fe
tupm <tupm@ows.com.vn>
2017-11-16 14:54:41 +0700

    asdf

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,6 +1,8 @@
 var config = require('../../config')
     , Sequelize = require('sequelize')
     , sequelize = new Sequelize(config.db, config.user, config.pass, {
+        connectTimeout: 20000,
+        adquireTimeout: 20000,
         host: "sql12.freemysqlhosting.net",
         dialect: config.dialect,
         port: config.db_port,
